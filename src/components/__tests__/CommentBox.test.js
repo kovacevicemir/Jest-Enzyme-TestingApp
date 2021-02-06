22380d998e5c2330cfecc7538e94855ffdcc94d7
emirkovacevic <emir.kovacevic@jjryan.com.au>
2021-02-06 22:58:46 +1000

    submit form and check if input is empty (reseted)

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -29,3 +29,22 @@ it("has a text area that users can type in", () => {
     //check if updated value is correct
     expect(wrapped.find("textarea").prop("value")).toEqual("new comment")
 });
+
+it("has text area, user can type in and submit",()=>{
+    //Find text area and simulate new value input
+    wrapped.find('textarea').simulate('change',{
+        target:{value:'new comment'}
+    });
+
+    //force component to update (setState)
+    wrapped.update();
+
+    //check if updated value is correct
+    expect(wrapped.find("textarea").prop("value")).toEqual("new comment")
+
+    //submit the form
+    wrapped.find("form").simulate('submit')
+
+    //check if textarea is clear
+    expect(wrapped.find("textarea").prop("value")).toEqual("")
+})
