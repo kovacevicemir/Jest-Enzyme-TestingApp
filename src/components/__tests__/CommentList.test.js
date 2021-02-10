1ab067c99ce3339410288fb1184edf53d567dc55
emirkovacevic <emir.kovacevic@jjryan.com.au>
2021-02-10 20:59:15 +1000

    finalize redux and mount

--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -18,5 +18,10 @@ beforeEach(() => {
 });
 
 it("creates one <li> per comment", () => {
-    console.log(wrapped.find('li').length)
+    expect(wrapped.find('li').length).toEqual(2);
 });
+
+it("shows the text for each comment",()=>{
+    expect(wrapped.render().text()).toContain("comment 1")
+    expect(wrapped.render().text()).toContain("comment 2")
+})
